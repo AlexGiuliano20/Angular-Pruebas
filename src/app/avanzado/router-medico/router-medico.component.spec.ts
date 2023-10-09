8240f7ebbb7956d3b59ff303c5b13b7543a0ba9d
AlexGiuliano20 <alex.giuliano37@gmail.com>
2023-10-08 23:45:59 -0300

    Comprobar parametros enviados a un observable

--- src/app/avanzado/router-medico/router-medico.component.spec.ts
+++ src/app/avanzado/router-medico/router-medico.component.spec.ts
@@ -1,6 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RouterMedicoComponent } from './router-medico.component';
+import { Observable, of } from 'rxjs';
+import { Subject } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
+
+class FakeRouter {
+  navigate(params: any) {}
+}
+
+class FakeActivatedRoute {
+  // params: Observable<any> = of({});
+  private _subject = new Subject();
+
+  push(valor: any) {
+    this._subject.next(valor);
+  }
+
+  get params() {
+    return this._subject.asObservable();
+  }
+}
 
 describe('RouterMedicoComponent', () => {
   let component: RouterMedicoComponent;
@@ -8,16 +28,33 @@ describe('RouterMedicoComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RouterMedicoComponent ]
-    })
-    .compileComponents();
+      declarations: [RouterMedicoComponent],
+      providers: [
+        { provide: Router, useClass: FakeRouter },
+        { provide: ActivatedRoute, useClass: FakeActivatedRoute },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(RouterMedicoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('Debe de redireccionar a Médico cuandos se guarde', () => {
+    const router = TestBed.inject(Router);
+    const spy = spyOn(router, 'navigate');
+
+    component.guardarMedico();
+
+    expect(spy).toHaveBeenCalledWith(['medico', '123']);
+  });
+
+  it('Debe de colocar el id = nuevo', () => {
+    component = fixture.componentInstance;
+
+    const activatedRoute = TestBed.inject(ActivatedRoute);
+    (<FakeActivatedRoute>(<any>activatedRoute)).push({ id: 'nuevo' });
+
+    expect(component.id).toBe('nuevo');
   });
 });
